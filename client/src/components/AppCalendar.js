22e84fec5667e91a2326e1133c47bb7b6451d5be
lesley <lesleyiannarelli@gmail.com>
2019-11-26 15:14:10 -0500

    minor changed to state and function

--- client/src/components/AppCalendar.js
+++ client/src/components/AppCalendar.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import Calendar from 'react-calendar';
 
 class AppCalendar extends Component {
-  state = {
-    date: new Date(),
+  constructor(props) {
+    super(props);
+    let today = new Date();
+    let yy = today.getFullYear();
+    let mm = today.getMonth() + 1;
+    let dd = today.getDate();
+    let wholeDate = mm + '/' + dd + '/' + yy;
+
+    state = {
+      date: new Date(),
+      newDate: wholeDate,
+    }
+    this.onChange = this.onChange.bind(this);
   }
 
-  onChange = date => this.setState({ date })
+  onChange = date => {
+    let yy = date.getFullYear();
+    let mm = date.getMonth() + 1;
+    let dd = date.getDate();
+    let concatDate = mm + '/' + dd + '/' + yy;
+    this.setState({
+      date: date,
+      newDate: concatDate
+     });
+  }
 
   render() {
     //const { value } = this.state;
@@ -15,6 +35,7 @@ class AppCalendar extends Component {
         <Calendar
           onChange={this.onChange}
           value={this.state.date}
+          newDate={this.state.newDate}
         />
       </div>
     );
