1c5fdd11cfad4620b5e1f4d4383ccaa82a2fc0d5
Derek <39328219+dpadilla1@users.noreply.github.com>
2019-12-02 13:25:58 -0800

    Reformatted ScheduleVisits. Made 'select ADL list' work.

--- client/src/views/CareManager/ScheduleVisits.js
+++ client/src/views/CareManager/ScheduleVisits.js
@@ -8,12 +8,21 @@ import axios from "axios";
 export default class ScheduleVisits extends React.Component {
     constructor(props) {
       super(props);
-      this.state = { modal: false,name: '',email: '', selectList: [], date: Date};
+      this.state = { 
+        modal: false,
+        name: '',
+        email: '', 
+        selectList: [], 
+        date: Date,
+        currentCareGiver: '',
+        selectedListName: ''
+      };
   
       this.toggle = this.toggle.bind(this);
       this.handleChangeName = this.handleChangeName.bind(this);
       this.handleChangeEmail = this.handleChangeEmail.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.onChangeSelectedListName = this.onChangeSelectedListName.bind(this);
 
 
     }
@@ -26,12 +35,20 @@ export default class ScheduleVisits extends React.Component {
 
         }
 
+      this.toggle();
+
 
     }
 
     componentDidMount() {
-        this.updateCustomLists();
+        //this.updateCustomLists();
         this.setState({date: Date()});
+    }
+
+    componentDidUpdate(prevProps) {
+      if(this.props.currentManager !== prevProps.currentManager) {
+        this.updateCustomLists();
+      }
     }
 
     toggle() {
@@ -58,10 +75,11 @@ export default class ScheduleVisits extends React.Component {
                 currentCareGiver: _id
             }
         );
-        console.log("Current Care Giver ID: ", _id);
+        console.log("Current SELECTED Care Giver ID: ", _id);
     }
 
     updateCustomLists(callback) {
+        console.log("IS THIS WORKING: ", this.props.currentManager);
         axios.get('http://localhost:5000/api/managers/'+ this.props.currentManager)
             .then(res => {
                 this.setState({
@@ -75,75 +93,79 @@ export default class ScheduleVisits extends React.Component {
             });
         if(callback) callback();
     }
+
+    onChangeSelectedListName(e) {
+      this.setState({
+        selectedListName: e.target.value
+      }, () => {
+        console.log("Current SELECTED list: ", this.state.selectedListName);
+      })
+    }
   
   
     render() {
       return (
   
           <div>
-          <Button color="secondary"  onClick={this.toggle} block>Schedule Visits</Button>
+          <Button color="secondary"  onClick={this.toggle} block>Schedule Visit</Button>
           <Modal isOpen={this.state.modal}>
-          <form onSubmit={this.handleSubmit}>
-            <ModalHeader>Schedule Visits</ModalHeader>
-            <ModalBody>
-            <div className="row">
-              <div className="form-group col-md-4">
-              <label>Caregiver:</label>
-              <CareGiverSelect changeCurrentCareGiver={this.changeCurrentCareGiver.bind(this)}/>
-                </div>
-                </div>
-             
-              <div className="row">
-                </div>
-                <Form>
-            <FormGroup>
-            <Label for="InputDate">Date</Label>
-                <Input
-                type="date"
-                name="date"
-                id="InputDate"
-                value={this.state.date}
-                placeholder="date placeholder"
-                />
-                <Label for="Time">From:</Label>
-                <Input
-                type="time"
-                name="time"
-                id="Time"
-                placeholder="time placeholder"
-                />
-                <Label for="Time">To:</Label>
-                <Input
-                type="time"
-                name="time"
-                id="Time"
-                placeholder="time placeholder"
-                />
-                <label>Saved ADL List: </label>
-                <select ref="listInput"
-                        required
-                        className="form-control"
-                        value={this.state.selectedListName}>
-                    {
-                        this.state.selectList.map(function(item) {
-                            return <option key={item._id} value={item.name}>
-                                {item.name}
-                            </option>;
-                        })
-                    }
-                </select>
+              <ModalHeader>Schedule Visit</ModalHeader>
 
-                <Label for="Text">Notes to Caregiver:</Label>
-                <Input type="textarea" name="text" id="exampleText" />
-            </FormGroup>
-            </Form>
-            </ModalBody>
-            <ModalFooter>
-              <input type="submit" value="Submit" color="primary" className="btn btn-primary"
-                     onClick={this.addVisit.bind(this)}/>
-              <Button color="danger" onClick={this.toggle}>Cancel</Button>
-            </ModalFooter>
-            </form>
+              <ModalBody>
+                <div className="form-row">
+                  <div className="form-group col-md-6">
+                    <CareGiverSelect changeCurrentCareGiver={this.changeCurrentCareGiver.bind(this)}/>
+                  </div>
+                </div>
+                
+                <Form>
+                  <FormGroup>
+                    <Label for="InputDate">Date</Label>
+                    <Input
+                      type="date"
+                      name="date"
+                      id="InputDate"
+                      value={this.state.date}
+                      placeholder="date placeholder"
+                    />
+                    <Label for="Time">From:</Label>
+                    <Input
+                      type="time"
+                      name="time"
+                      id="Time"
+                      placeholder="time placeholder"
+                    />
+                    <Label for="Time">To:</Label>
+                    <Input
+                      type="time"
+                      name="time"
+                      id="Time"
+                      placeholder="time placeholder"
+                    />
+                    <label>ADL List: </label>
+                    <select ref="listInput"
+                      required
+                      className="form-control"
+                      value={this.state.selectedListName}
+                      onChange={this.onChangeSelectedListName} >
+                    {
+                      this.state.selectList.map(function(item) {
+                          return <option key={item._id} value={item.name}>
+                              {item.name}
+                            </option>;
+                      })
+                    }
+                    </select>
+                    <Label for="Text">Notes to Caregiver:</Label>
+                    <Input type="textarea" name="text" id="exampleText" />
+                  </FormGroup>
+                </Form>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button color="primary" onClick={this.addVisit.bind(this)}>Submit</Button>
+                <Button color="danger" onClick={this.toggle}>Cancel</Button>
+              </ModalFooter>
           </Modal>
           </div>
         
