78b518eb348427bbdb836a35caa9e7ce34b0898c
lesley <lesleyiannarelli@gmail.com>
2019-12-08 15:43:10 -0500

    calendar shows visits for the day

---
+++ client/src/views/Caregiver/CalendarArea.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import Calendar from 'react-calendar'
+
+class CalendarArea extends React.Component {
+  constructor(props) {
+    super(props);
+    let today = new Date();
+    let yy = today.getFullYear();
+    let mm = today.getMonth() + 1;
+    let dd = today.getDate();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    let wholeDate = yy + '-' + mm + '-' + dd;
+    let hh = today.getHours();
+    let min = today.getMinutes();
+    let wholeTime = hh + ':' + min;
+
+    this.state = {
+      date: new Date(),
+      newDate: wholeDate,
+      newTime: wholeTime,
+    }
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange = date => {
+    let yy = date.getFullYear();
+    let mm = date.getMonth() + 1;
+    let dd = date.getDate();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    let concatDate = yy + '-' + mm + '-' + dd;
+    this.setState({
+      date: date,
+      newDate: concatDate
+     });
+  }
+
+    render() {
+
+      const divStyle = {
+        float: 'left',
+        marginRight: '2em'
+      }
+
+      const visitStyle = {
+        fontSize: '15px',
+      }
+
+      var visits;
+
+      if(this.props.visits !== null) {
+        visits = this.props.visits.map((v,i) => {
+          let thisVisit = v.scheduledDate;
+          if(this.state.newDate === thisVisit) {
+            return (
+              <div className="text-left" key={i}>
+                <h6>Visit on {v.scheduledDate}</h6>
+                <ul key={i}>
+                  <li>With Patient#: {v.patient}</li>
+                  <li>Start at: {v.scheduledStartTime}</li>
+                  <li>End at: {v.scheduledFinishTime}</li>
+                  <li>Notes: {v.managerNotes}</li>
+                  <li>ADL List Order: {v.ADLlist.order.map(n => n + " ")}</li>
+                </ul>
+              </div>
+            )
+          }
+        })
+      }
+
+      return (
+        <div>
+          <div style={divStyle}>
+            <Calendar
+              onChange={this.onChange}
+              value={this.state.date}
+            />
+          </div>
+
+          <div>
+            <h6>Visits:</h6>
+            <div style={visitStyle}>
+              {visits}
+            </div>
+          </div>
+        </div>
+      )
+    }
+}
+
+export default CalendarArea;
